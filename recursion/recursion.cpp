aca5381eb1b7f004beac2d705d891b6d5df8290d
valmarfina <valmarfina@gmail.com>
2020-11-15 19:12:55 +0300

    1.1

--- recursion/recursion.cpp
+++ recursion/recursion.cpp
@@ -1,10 +1,10 @@
 ﻿#include <iostream>
 #include <fstream>
 
-/*
+//ВАРИАНТ 13
 
-Напишите программу, выполняющую ввод строк из файла и проверку соответствия каждой введенной строки правилу
-(в соответствии с указанным вариантом).
+/*
+Напишите программу, выполняющую ввод строк из файла и проверку соответствия каждой введенной строки правилу (в соответствии с указанным вариантом).
 Входные данные: Символьная строка. Ввести из файла потока cin.
 Выходные данные: «ДА», если строка соответствует правилу, «НЕТ», если строка НЕ соответствует правилу.
 
@@ -28,134 +28,146 @@
 bool isExpression(char**);
 bool isTerm(char**);
 bool isMultiplier(char**);
-bool isInteger(char**);
+bool isNumber(char**);
 bool isDigit(char**);
 bool isLetter(char**);
 
-//<буква> ::= a | b | c | d | e | f | x | y | z
 bool isLetter(char** s)
 {
-	if ((**s >= 'a' && **s <= 'f') || (**s >= 'x' && **s <= 'z')) {
-		*s += 1;
-		return true;
-	}
-	else
-		return false;
+    if ((**s >= 'a') && (**s <= 'f') || (**s >= 'x') && (**s <= 'z'))
+    {
+        *s = *s + 1;
+        return true;
+    }
+    return false;
 }
 
-//<цифра> ::= 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 bool isDigit(char** s)
 {
-	if (**s >= '0' && **s <= '9')
-	{
-		*s += 1;
-		return true;
-	}
-	return false;
+    if (**s >= '0' && **s <= '9')
+    {
+        *s = *s + 1;
+        return true;
+    }
+    return false;
 }
 
-//<целое без знака> :: = <цифра> | <цифра> <целое без знака>
-bool isInteger(char** s)
+bool isNumber(char** s)
 {
-	if (isDigit(s))
-	{
-		isInteger(s);
-		return true;
-	}
-	return false;
+    if (isDigit(s))
+    {
+        isNumber(s);
+        return true;
+    }
+    return false;
 }
 
-//<множитель> ::= <целое без знака> | <буква> | (<выражение>)
+
+
 bool isMultiplier(char** s)
 {
-	if (isInteger(s) || isLetter(s))
-	{
-		return true;
-	}
-	if (**s == '(')
-	{
-		*s += 1;
-		if (isExpression(s))
-		{
-			if (**s == ')')
-			{
-				*s += 1;
-				return true;
-			}
-		}
-	}
-	return false;
+    if (isLetter(s))
+        return true;
+    else if (isNumber(s))
+        return true;
+    else if (**s == '(')
+    {
+        *s = *s + 1;
+        if (isExpression(s))
+        {
+            if (**s == ')')
+            {
+                *s = *s + 1;
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+    return false;
 }
 
-//<терм> ::= <множитель> | (<множитель> * <терм>) |(<множитель> / <терм>)
-bool isTerm(char** s) {
-	if (isMultiplier(s))
-	{
-		return true;
-	}
-	else if (**s == '(')
-	{
-		*s += 1;
-		if (isMultiplier(s))
-		{
-			if (**s == '*' || **s == '/')
-			{
-				*s += 1;
-				if (isTerm(s))
-				{
-					if (**s == ')')
-					{
-						*s += 1;
-						return true;
-					}
-				}
-			}
-		}
-	}
-		return false;
+bool isTerm(char** s)
+{
+    if (isMultiplier(s))
+    {
+        if (**s == '*' || **s == '/')
+        {
+            *s = *s + 1;
+            return isTerm(s);
+        }
+        return true;
+    }
+    return false;
 }
 
-//<выражение> ::= <терм> | <терм> +<выражение> | <терм> – <выражение>
+//bool isTerm(char** s) {
+//    if (**s == '(')
+//    {
+//        *s += 1;
+//        if (isMultiplier(s))
+//        {
+//            if (**s == '*' || **s == '/')
+//            {
+//                *s += 1;
+//                if (isTerm(s))
+//                {
+//                    if (**s == ')')
+//                    {
+//                        *s += 1;
+//                        return true;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    else if (isMultiplier(s))
+//    {
+//        return true;
+//    }
+//    return false;
+//}
+
 bool isExpression(char** s)
 {
-	if (isTerm(s))
-	{
-		if (**s == '+' || **s == '-')
-		{
-			*s += 1;
-			return isExpression(s);
-		}
-		return true;
-	}
-	return false;
+    if (isTerm(s))
+    {
+        if (**s == '+' || **s == '-')
+        {
+            *s = *s + 1;
+            return isExpression(s);
+        }
+        return true;
+    }
+    return false;
 }
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
+    setlocale(LC_ALL, "rus");
 
-	const int size = 1000;
-	int count = 1;
+    const int size = 1000;
+    int count = 1;
 
-	std::ifstream fin("input.txt");
+    std::ifstream fin("input.txt");
 
-	while (!fin.eof())
-	{
-		char* s = new char[size + 1];
+    while (!fin.eof())
+    {
+        char* s = new char[size + 1];
 
-		//fin.ignore('\n');
-		fin.getline(s, size);
+        //fin.ignore('\n');
+        fin.getline(s, size);
 
-		char* s2 = s;
+        char* s2 = s;
 
-		std::cout << count << ". " << (isExpression(&s) && *s == '\0' ? "Да" : "Нет") << "\n";
+        std::cout << count << ". " << (isExpression(&s) && *s == '\0' ? "Да" : "Нет") << "\n";
 
-		s = s2;
-		count++;
+        s = s2;
+        count++;
 
-		delete[] s;
-	}
+        delete[] s;
+    }
 
-	fin.close();
-	return 0;
+    fin.close();
+    return 0;
 }
